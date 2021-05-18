f8573f5dd40f42dd0e0a0ad59ce05e63336315d5
Joao Pedro <joaopedrowatson@hotmail.com>
2021-05-18 14:57:19 -0300

    separação dos dados

--- connection.py
+++ connection.py
@@ -1,5 +1,7 @@
 import sqlite3
 from sqlite3 import Error
+# https://lucid.app/lucidchart/037c7067-c87a-4132-8268-8299d0494652/edit?beaconFlowId=1D5D81D3CCE07A60&page=BUMDxnZpKJ2k#
+FILE = 'database.db'
 
 
 def create_connection(db_file):
@@ -16,3 +18,21 @@ def create_connection(db_file):
         print(e)
 
     return conn
+
+
+def insert(*args):
+    conn = create_connection(FILE)
+
+    try:
+        cursor = conn.cursor()
+        query = """INSERT INTO stock
+                            (id, name, ticket, name, active) 
+                            (?, ?, ?, ?)"""
+        
+        
+    except Error as e:
+        print(f'Error in database operation, {e}')
+    finally:
+        cursor.close()
+        
+
--- main.py
+++ main.py
@@ -1,24 +1,53 @@
 import os
-import requests
+from rich import print
+from datetime import date
+from datetime import timedelta
 from dotenv import load_dotenv
-from pandas.core.frame import DataFrame
-from requests.exceptions import HTTPError
+from typing import Tuple, NewType
 from connection import create_connection
 from alpha_vantage.timeseries import TimeSeries
 
 load_dotenv()
 APIKEY: str = os.getenv('API_TOKEN')
+today: date = date.today()
+last_week: date = date.today() - timedelta(days=7)
 
 
-def get_api_data(symbol: str) -> DataFrame:
+def get_last_week(symbol: str) -> list:
+    """get data from alpha_vantage api
+
+    Args:
+        symbol (str): symbol reference to stock example: B3SA3.SAO/PETR4.SAO
+
+    Returns:
+        list: Array contains values from last week about passed stock
+    """
+
+    last_week_values: tuple[dict[str, str]] = []
+    
     ts: TimeSeries = TimeSeries(key=APIKEY, output_format='json')
-    data: DataFrame = ts.get_daily(symbol=symbol, outputsize='full')
+    data, _ = ts.get_daily(symbol=symbol, outputsize='full')
+
+    for i in range(0, 8):
+        try:
+            last_week_values.append(
+                {str(last_week + timedelta(days=i)): data[str(last_week + timedelta(days=i))]['4. close']})
+        except KeyError:
+            pass
+
+    return last_week_values
+
+
+def get_all_data(symbol):
+    ts = TimeSeries(key=APIKEY, output_format='json')
+    data, _ = ts.get_daily(symbol=symbol, outputsize='full')
 
     return data
 
 
 if __name__ == '__main__':
-    options = ['B3SA3.SAO', 'PETR4.SAO']
-
-    result = get_api_data(options[1])
+    stocks: str = ['B3SA3.SAO', 'PETR4.SAO']
+    all_result = get_all_data(stocks[0])
+    result = get_last_week(stocks[0])
+    
     print(result)
